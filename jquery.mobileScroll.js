01a19e352b3c4a1af3290075d59b585ff2ba2516
Kang-min Liu <gugod@gugod.org>
2010-05-23 16:19:34 +0800

    add horizontal scroll

--- jquery.mobileScroll.js
+++ jquery.mobileScroll.js
@@ -5,19 +5,28 @@ $.fn.mobileScroll = function() {
         var e = event.originalEvent.targetTouches[0];
         $(this).data('startY', e.pageY);
         $(this).data('scrollTop', $(this).scrollTop());
+        $(this).data('startX', e.pageX);
+        $(this).data('scrollLeft', $(this).scrollLeft());
         $(this).data("t", Date.now());
     }).live("touchmove", function(event) {
         event.preventDefault();
         var e = event.originalEvent.targetTouches[0];
+
+        var startX = $(this).data('startX');
         var startY = $(this).data('startY');
+
+        var dx = startX - e.pageX;
         var dy = startY - e.pageY;
-        $(this).scrollTop( $(this).data('scrollTop') + dy );
+
+        $(this).scrollTop(  $(this).data('scrollTop') + dy );
+        $(this).scrollLeft( $(this).data('scrollLeft') + dx );
+
         var dt= Date.now() - $(this).data("t");
         $(this).data("s", parseInt(1000*dy/dt));
         return false;
     }).live("touchend", function() {
         var s = $(this).data("s");
-        $(this).animate({scrollTop: "+=" + s + "px"}, 1000, "easeOutCirc");
+        $(this).animate({scrollTop: "+=" + s + "px", scrollLeft: "+=" + s + "px"}, 1000, "easeOutCirc");
     });
 
     return this;
